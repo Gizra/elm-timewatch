04e936989ce1d9186adc888d12484b7586a28116
Itamar Shapira Bar-Lev <itamar@gizra.com>
2015-11-11 14:00:03 +0200

    Implementing numbers keypad

--- src/elm/Hi.elm
+++ src/elm/Hi.elm
@@ -2,15 +2,32 @@ module Hi where
 
 import Effects exposing (Effects)
 import Html exposing (..)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Http exposing (Error)
+import String exposing (length, repeat)
+import Task exposing (Task, succeed)
 
+import Debug
 
 -- MODEL
 
-type alias Model = String
+type Status =
+  Init
+  | Fetching
+  | Fetched
+  | HttpError Http.Error
+
+type alias Model =
+  { pinCode : String
+  , status : Status
+  }
 
 initialModel : Model
-initialModel = ""
+initialModel =
+  { pinCode = ""
+  , status = Init
+  }
 
 init : (Model, Effects Action)
 init =
@@ -21,25 +38,47 @@ init =
 
 -- UPDATE
 
-type Action = AddDigit Int
+type Action
+  = AddDigit Int
+  | SubmitCode
 
 update : Action -> Model -> (Model, Effects Action)
 update action model =
   case action of
     AddDigit digit ->
-      ( model ++ toString(digit)
-      , Effects.none
-      )
+      let
+        pinCode' =
+          if length model.pinCode < 4
+            then model.pinCode ++ toString(digit)
+            else ""
+        effects' =
+          if length model.pinCode == 3
+            then Task.succeed SubmitCode |> Effects.task
+            else Effects.none
+      in
+        ( { model | pinCode <- pinCode' }
+        , effects'
+        )
 
+    SubmitCode ->
+      let
+        _ = Debug.log model.pinCode True
+      in
+        ( { model | pinCode <- "" }
+        , Effects.none )
 
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
-    [ button [ onClick address (AddDigit 1) ] [ text "1" ]
-    , button [ onClick address (AddDigit 2) ] [ text "2" ]
-    , button [ onClick address (AddDigit 3) ] [ text "3" ]
-    , button [ onClick address (AddDigit 4) ] [ text "4" ]
-    , div [ ] [ text (toString model) ]
+  div
+    [ class "number-pad" ]
+    [ div
+        [ class "number-buttons" ]
+        ( List.map (digitButton address) [0..9] |> List.reverse )
+        , div [] [ text <| repeat (length model.pinCode) "*" ]
     ]
+
+digitButton : Signal.Address Action -> Int -> Html
+digitButton address digit =
+  button [ onClick address (AddDigit digit) ] [ text <| toString digit ]
