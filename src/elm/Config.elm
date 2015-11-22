79728bc3c793b257ffa820b317ee8e9ac307aa2c
YaronMiro <yaron@gizra.com>
2015-11-22 10:25:24 +0200

    update accessToken

--- src/elm/Config.elm
+++ src/elm/Config.elm
@@ -4,4 +4,4 @@ backendUrl : String
 backendUrl = "http://localhost/productivity/www"
 
 accessToken : String
-accessToken = "V1q0fF5ML1h5-424-dlPgIXScQ_hDAbrUrDlVVpB35s"
+accessToken = "NCIUsh7K-iFfqZ46OQ8_RXZ2qvFy5mhYBbFqcNLTef4"
